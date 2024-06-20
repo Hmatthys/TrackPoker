4fcb81e749f614357f7b32f084bf08a917e2df81
matth637 <matth637@umn.edu>
2024-06-20 16:27:32 -0500

    giving up for now

--- app/page.tsx
+++ app/page.tsx
@@ -16,14 +16,14 @@ export default async function Index() {
       return (
 
           <div style={{ width: 'clamp(300px, 100%, 900px', margin: '0 auto', padding: '1rem'}}>
-            <TabView scrollable={true}>
-              <TabPanel header="Players" headerStyle={{width: '20%',  backgroundColor: '#202c34' }}>
+            <TabView>
+              <TabPanel header="Players" headerStyle={{marginLeft: '0.2rem', width: '20%',  backgroundColor: '#202c34', borderTopRightRadius: '16px', borderTopLeftRadius: '16px' }}>
                 <PlayerTable />
               </TabPanel>
-              <TabPanel header="Games" headerStyle={{  width: '20%',  backgroundColor: '#202c34' }}>
+              <TabPanel header="Games" headerStyle={{marginLeft: '0.2rem',  width: '20%',  backgroundColor: '#202c34' }}>
                 <GameTable />
               </TabPanel>
-              <TabPanel header="Chart" headerStyle={{ width: '20%', backgroundColor: '#202c34' }}>
+              <TabPanel header="Chart" headerStyle={{ marginLeft: '0.2rem', width: '20%', backgroundColor: '#202c34', borderBottomRightRadius: '16px', borderBottomLeftRadius: '16px' }}>
                 <TotalChart />
               </TabPanel>
             </TabView>
