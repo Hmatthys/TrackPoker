0acf0fa106e4b026f5001c31e773be9eea491a57
matth637 <matth637@umn.edu>
2024-06-20 16:06:46 -0500

    another attempt

--- app/page.tsx
+++ app/page.tsx
@@ -17,13 +17,13 @@ export default async function Index() {
 
           <div style={{ width: 'clamp(300px, 100%, 900px', margin: '0 auto', padding: '1rem'}}>
             <TabView>
-              <TabPanel header="Players" headerStyle={{   margin: '15px', backgroundColor: '#202c34', float: 'left' }}>
+              <TabPanel header="Players" headerStyle={{   display:'flex', backgroundColor: '#202c34' }}>
                 <PlayerTable />
               </TabPanel>
-              <TabPanel header="Games" headerStyle={{   margin: '15px', backgroundColor: '#202c34' }}>
+              <TabPanel header="Games" headerStyle={{   display:'flex', margin: '15px', backgroundColor: '#202c34' }}>
                 <GameTable />
               </TabPanel>
-              <TabPanel header="Chart" headerStyle={{  margin: '15px', backgroundColor: '#202c34', float: 'right' }}>
+              <TabPanel header="Chart" headerStyle={{  display:'flex', margin: '15px', backgroundColor: '#202c34' }}>
                 <TotalChart />
               </TabPanel>
             </TabView>
