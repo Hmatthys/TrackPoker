1d4b4644ba4fed65ea1e16b8af2b2baf29668ccd
matth637 <matth637@umn.edu>
2024-11-20 15:20:41 -0600

    still bug finding

--- components/PlayerTable.tsx
+++ components/PlayerTable.tsx
@@ -44,10 +44,10 @@ export default async function Index() {
     let count = 1;
     let playerData = new Array<[number, string, number, number, number]>;
     player.map((e) => {
-        playerData.push([e.playerid, e.name, e.number_of_sessions, e.profit, count]);
-        currentPlace[e.playerid - 1][0] = count;
-        currentPlace[e.playerid - 1][2] = e.profit;
-        count += 1;
+        // playerData.push([e.playerid, e.name, e.number_of_sessions, e.profit, count]);
+        // currentPlace[e.playerid - 1][0] = count;
+        // currentPlace[e.playerid - 1][2] = e.profit;
+        // count += 1;
     })
     
     // lastGame.map((e) => 
