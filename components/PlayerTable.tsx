f9016d6fba79b51bc184d50d2d06c9924e152cb2
matth637 <matth637@umn.edu>
2024-11-20 15:22:23 -0600

    line by line

--- components/PlayerTable.tsx
+++ components/PlayerTable.tsx
@@ -44,7 +44,7 @@ export default async function Index() {
     let count = 1;
     let playerData = new Array<[number, string, number, number, number]>;
     player.map((e) => {
-        // playerData.push([e.playerid, e.name, e.number_of_sessions, e.profit, count]);
+        playerData.push([e.playerid, e.name, e.number_of_sessions, e.profit, count]);
         // currentPlace[e.playerid - 1][0] = count;
         // currentPlace[e.playerid - 1][2] = e.profit;
         // count += 1;
