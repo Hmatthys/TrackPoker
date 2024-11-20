951ccdf0d169a6f05c82e4a18a08b5698c75b116
matth637 <matth637@umn.edu>
2024-11-20 13:55:25 -0600

    add logic for place tracking

--- components/PlayerTable.tsx
+++ components/PlayerTable.tsx
@@ -15,23 +15,68 @@ export default async function Index() {
   if(!player){
     return <p>No players</p>
   }
-
+  const {data: game, error: gameerror, count: numgames} = await supabase
+        .from('game')
+        .select('*', {count: 'exact', head: true})
+        if(gameerror){
+            return <p>Count error</p>
+        }
+  const {data: d, error: playererror, count: numplayers} = await supabase
+      .from('player')
+      .select('*', {count: 'exact', head: true})
+      if(playererror){
+          return <p>Count error</p>
+      }
+      if(!numplayers){
+        return <p>Count error</p>
+      }
+  const { data: lastGame } = await supabase
+    .from('sessions')
+    .select('player, game, profit')
+    .eq('game', numgames);
+    
+    
+    let currentPlace =  new Array<[number, number, number]>(numplayers)
+    let count = 1
+    let playerData = new Array<[number, string, number, number, number]>
+    player.map((e) => {
+        playerData.push([e.playerid, e.name, e.number_of_sessions, e.profit, count])
+        currentPlace[e.playerid][0] = count;
+        currentPlace[e.playerid][2] = e.profit;
+        count += 1;
+    })
+    lastGame?.map((e) => {
+      currentPlace[e.player][2] -= e.profit;
+    })
+    currentPlace.sort((a, b) => b[2] - a[2])
+    count = 0
+    currentPlace.map((e) => {
+        currentPlace[count][1] = count + 1;
+        count += 1;
+    })
+    let updatedPlace =  new Array<number>(numplayers)
+    count = 0
+    currentPlace.map((e) => {
+      updatedPlace[e[0]] = e[0] - e[1] 
+    })
       return (     
     <div style={{  padding: '10px', margin: '15px', width: '100%', fontSize: '1.2em', backgroundColor: '#202c34', borderRadius: '16px' }}>
     
     <table style={{    width: '100%' }}>
         <tr>
-        <th style = {{textAlign: 'left', width: '50%', borderBottom: '1px', borderColor: 'white'}}>Player</th>
+        <th style = {{textAlign: 'left', width: '40%', borderBottom: '1px', borderColor: 'white'}}>Player</th>
         <th style = {{textAlign: 'center'}}>Profit</th> 
         <th style = {{textAlign: 'center'}}>Games Played</th>
         <th style = {{textAlign: 'center'}}>Average Profit</th>
+        <th style = {{textAlign: 'center'}}>Change</th>
         </tr>
-            {player.map((player) => (
-            <tr key={player.playerid} style= {{borderBottom: '1px solid gray'}}>
-            <td style = {{borderBottom: '1px', borderColor: 'white'}}><Link href={`/players/${player.playerid}`}>{player.name}</Link></td>
-            <td style = {{textAlign: 'center', borderBottom: '1px', borderColor: 'white'}}>{player.profit}</td>
-            <td style = {{textAlign: 'center', borderBottom: '1px', borderColor: 'white'}}>{player.number_of_sessions}</td>
-            <td style = {{textAlign: 'center', borderBottom: '1px', borderColor: 'white'}}>{Math.floor(player.profit / player.number_of_sessions)}</td>
+            {playerData.map((player) => (
+            <tr key={player[0]} style= {{borderBottom: '1px solid gray'}}>
+            <td style = {{borderBottom: '1px', borderColor: 'white'}}><Link href={`/players/${player[0]}`}>{player[1]}</Link></td>
+            <td style = {{textAlign: 'center', borderBottom: '1px', borderColor: 'white'}}>{player[3]}</td>
+            <td style = {{textAlign: 'center', borderBottom: '1px', borderColor: 'white'}}>{player[2]}</td>
+            <td style = {{textAlign: 'center', borderBottom: '1px', borderColor: 'white'}}>{Math.floor(player[3] / player[2])}</td>
+            <td style = {{textAlign: 'center', borderBottom: '1px', borderColor: 'white'}}>{updatedPlace[player[4]]}</td>
             </tr>))} 
         
         </table>  
