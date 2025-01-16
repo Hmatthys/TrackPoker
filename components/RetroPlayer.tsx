6a39ceea1c4b11f4bfeaad41fdb1c945af161022
matth637 <matth637@umn.edu>
2025-01-15 18:25:25 -0600

    fix player charts from noncurrent year

---
+++ components/RetroPlayer.tsx
@@ -0,0 +1,102 @@
+
+import { createClient } from "@/utils/supabase/server";
+
+import Link from 'next/link';
+import React from 'react';
+
+
+// DONT NEED TO CALCULATE CHANGE ON A NON ALL TIME NON CURRENT YEAR STAT PAGE
+
+export default async function Index( {year} : {year:number} ) {
+
+      const supabase = createClient();
+
+      const YEAR = year
+
+      const {data: games, error: gameserror} = await supabase
+            .from('game')
+            .select()
+            .gt('gamedate', YEAR * 10000)
+            .lt('gamedate', (YEAR + 1) * 10000)
+        if(gameserror){
+            return <p>Games error</p>
+        }
+        
+        // Find the gameid of the first game of the year, all gameids after this will be of current year
+        games.sort((a,b) => a.gamedate < b.gamedate ? -1 : a.gamedate > b.gamedate ? 1 : 0)
+
+        const startGame = games[0].gameid
+        const endGame = games[-1].gameid
+
+
+
+      const { data: player } = await supabase
+      .from('orderedresults')
+      .select('playerid, name');
+
+
+      if(!player){
+        return <p>No players</p>
+      }
+
+   
+      let playerData = new Array<[number, string, number, number, number]>; // ID NAME NUM_GAMES PROFIT CHANGE
+     for(let i = 0; i < player.length; i++){
+        let {data: playerSessions, error: sessionerror} = await supabase
+          .from('sessions')
+          .select('profit')
+          .eq('player', player[i].playerid)
+          .gte('game', startGame)
+          .lte('game', endGame);
+
+        if(sessionerror){
+          return <p>Session Error</p>
+        }
+        if(!playerSessions){
+          continue
+        }
+        let profitSum = 0;
+        for(let j = 0; j < playerSessions.length; j++){
+          profitSum += playerSessions[j].profit;
+        }
+        playerData.push([player[i].playerid, player[i].name, playerSessions.length, profitSum,  50])
+
+     }
+
+     
+     playerData.sort((a, b) => a[3] > b[3] ? -1 : a[3] < b[3] ? 1 : 0)
+
+    
+     
+
+    
+
+   
+      
+
+          return (     
+        <div style={{  padding: '10px', margin: '15px', width: '100%', fontSize: '1.2em', backgroundColor: '#202c34', borderRadius: '16px' }}>
+        
+        <table style={{    width: '100%' }}>
+            <tr>
+            <th style = {{textAlign: 'left', width: '40%', borderBottom: '1px', borderColor: 'white'}}>Player</th>
+            <th style = {{textAlign: 'center'}}>Profit</th> 
+            <th style = {{textAlign: 'center'}}>Games Played</th>
+            <th style = {{textAlign: 'center'}}>Average Profit</th>
+            </tr>
+                {playerData.map((player) => (
+                <tr key={player[0]} style= {{borderBottom: '1px solid gray'}}>
+                <td style = {{borderBottom: '1px', borderColor: 'white'}}><Link href={`/players/${player[0]}`}>{player[1]}</Link></td>
+                <td style = {{textAlign: 'center', borderBottom: '1px', borderColor: 'white'}}>{player[3]}</td>
+                <td style = {{textAlign: 'center', borderBottom: '1px', borderColor: 'white'}}>{player[2]}</td>
+                <td style = {{textAlign: 'center', borderBottom: '1px', borderColor: 'white'}}>{Math.floor(player[3] / player[2])}</td>
+                </tr>))} 
+            
+            </table>  
+        </div>
+                  
+
+                  
+          );
+}
+      
--- components/Yearly.tsx
+++ components/Yearly.tsx
@@ -3,18 +3,35 @@ import { notFound } from 'next/navigation';
 import YearlyPlayer from "@/components/YearlyPlayer";
 import YearlyGame from "@/components/YearlyGame";
 import YearlyChart from "@/components/YearlyChart";
+import RetroPlayer from "@/components/RetroPlayer";
 
 
-
+const REALYEAR = 25
 
 
 export default async function Index({year} : {year:number}) {
 
-
-    return (
+    if(year == REALYEAR){
+      return (
         <div>
             <div>
                 <YearlyPlayer year={year} />   
+            </div>
+          
+            <div>
+                <YearlyGame year={year} />
+            </div>
+            <div>
+                <YearlyChart year={year}/>
+            </div>
+        </div>
+       
+    )
+    }
+    return (
+        <div>
+            <div>
+                <RetroPlayer year={year} />   
             </div>
             <div>
                 <YearlyGame year={year} />
