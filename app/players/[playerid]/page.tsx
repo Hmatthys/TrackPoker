95020070002c7e1f2f1af56d87f802fa5a4ca745
Henry Matthys <2684773m@student.gla.ac.uk>
2025-12-23 17:17:57 -0600

    comment out playerid page see if it fixes

--- app/players/[playerid]/page.tsx
+++ app/players/[playerid]/page.tsx
@@ -18,47 +18,47 @@ const YEAR = 26
 export default async function Player({ params: { playerid } }: { params: { playerid: string } }) {
   const supabase = createClient();
 
-  const {data: games, error: gameserror} = await supabase
-    .from('game')
-    .select()
-    .gt('gamedate', YEAR * 10000);
-    if(gameserror){
-        return <p>Games error</p>
-    }
+  // const {data: games, error: gameserror} = await supabase
+  //   .from('game')
+  //   .select()
+  //   .gt('gamedate', YEAR * 10000);
+  //   if(gameserror){
+  //       return <p>Games error</p>
+  //   }
     // if(games.length == 0){
     //   return <LegacyPlayer id = { playerid }/> // If no games exist this year just return all time
     // }
 // Find the gameid of the first game of the year, all gameids after this will be of current year
-    games.sort((a,b) => a.gamedate < b.gamedate ? -1 : a.gamedate > b.gamedate ? 1 : 0)
+//     games.sort((a,b) => a.gamedate < b.gamedate ? -1 : a.gamedate > b.gamedate ? 1 : 0)
 
-    const startGame = games[0].gameid
-
-
-  const {data: results, error: resultserror} = await supabase
-    .from('orderedresults')
-    .select()
-    .eq('playerid', playerid)
-    .single();
+//     const startGame = games[0].gameid
 
 
-  if(resultserror){
-    return <p> results error</p>
-  }
+//   const {data: results, error: resultserror} = await supabase
+//     .from('orderedresults')
+//     .select()
+//     .eq('playerid', playerid)
+//     .single();
+
+
+//   if(resultserror){
+//     return <p> results error</p>
+//   }
       
   
 
-const {data: sessions, error: sessionerror} = await supabase
-    .from('sessions')
-    .select('game, profit')
-    .eq('player', playerid)
-    .gte('game', startGame);
-    if(sessionerror){
-      return <p>Session error</p>
-    }
+// const {data: sessions, error: sessionerror} = await supabase
+//     .from('sessions')
+//     .select('game, profit')
+//     .eq('player', playerid)
+//     .gte('game', startGame);
+//     if(sessionerror){
+//       return <p>Session error</p>
+//     }
 
-    if(results.latest_game < startGame){
-        return <LegacyPlayer id = { playerid }/> // IF NO GAMES PLAYED THIS YEAR JUST RETURN ALL TIME STATS
-    }
+//     if(results.latest_game < startGame){
+//         return <LegacyPlayer id = { playerid }/> // IF NO GAMES PLAYED THIS YEAR JUST RETURN ALL TIME STATS
+//     }
   
   return (
     <div style={{ width: 'clamp(300px, 100%, 900px', margin: '0 auto', padding: '1rem'}}>
